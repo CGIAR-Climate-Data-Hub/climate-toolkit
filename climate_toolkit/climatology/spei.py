4d6efd4ef5e45ce8ebf085015e4560f1fc71cfee
sammyjoseph999m <sammyjoseph999@gmail.com>
2026-07-08 12:47:17 +0300

    docs(spei): update example to renamed climate_toolkit module path

--- climate_toolkit/climatology/spei.py
+++ climate_toolkit/climatology/spei.py
@@ -738,7 +738,7 @@ if __name__ == "__main__":
 ##If you want the SPEI CLI command
 
 # Use:
-# python -m climate_tookit.climatology.spei --location "-1.286,36.817" --source agera_5 --start 2020-01-01 --end 2020-12-31 --index spei --scale-months 12
+# python -m climate_toolkit.climatology.spei --location "-1.286,36.817" --source agera_5 --start 2020-01-01 --end 2020-12-31 --index spei --scale-months 12
 
 # Or if installed as a console script:
 # Use:
